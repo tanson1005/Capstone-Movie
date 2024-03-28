53acce01eff9ab55591b253e01a51a4df294b33d
ngoduc <ngoduc202@gmail.com>
2024-03-28 22:56:15 +0700

    fix some bugs 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/configStore";
 
 // document.getElementById("root") as HTMLElement
 // Giúp typescript biết chắc chắn là HTMLElement
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <Fragment>
+        <Provider store={store}>
         <App />
-    </BrowserRouter>
+        </Provider>
+    </Fragment>
 
 );
